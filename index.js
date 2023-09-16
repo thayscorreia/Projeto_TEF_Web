28b14036ac6ae95d2cb5de78f32d1d15eadabc44
ThaysCorreia <thayscorreia.contato@gmail.com>
2023-09-16 16:36:12 -0300

    [feat] - tela de cadastro TEF

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.get("/",(req, res, next) => {
     res.render("index")
 })
 
+app.get("/cadastroTef", (req,res,next) =>{
+    res.render('cadastroTef')
+})
+
 app.listen(8082, () =>{
     console.log('app rodando na porta 8182')
 })
